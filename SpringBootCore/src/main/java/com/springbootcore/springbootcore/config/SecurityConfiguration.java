2e1f63dc92a6e6aab7c5abbc85480f678b52b212
dk <workplace.davidkariuki@gmail.com>
2022-06-02 17:09:03 +0300

    Updated security configuration.

--- SpringBootCore/src/main/java/com/springbootcore/springbootcore/config/SecurityConfiguration.java
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/config/SecurityConfiguration.java
@@ -10,15 +10,25 @@
 
 package com.springbootcore.springbootcore.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity webSecurity) throws Exception {
+		webSecurity.ignoring()
+				.antMatchers("/assets/**")
+				.antMatchers("/css/**");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -28,5 +38,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/index").permitAll()
 				.anyRequest().authenticated()
 				.and().csrf().disable();
+	}
+
+	@Bean
+	public AuthenticationSuccessHandler authenticationSuccessHandler() {
+		return new SimpleUrlAuthenticationSuccessHandler();
 	}
 }
