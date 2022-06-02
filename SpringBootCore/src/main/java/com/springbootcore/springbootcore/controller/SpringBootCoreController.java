3224d01ed1775fd0468db217de3f322b72741be4
dk <workplace.davidkariuki@gmail.com>
2022-06-02 14:26:17 +0300

    Added methods load index page with project details

--- SpringBootCore/src/main/java/com/springbootcore/springbootcore/controller/SpringBootCoreController.java
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/controller/SpringBootCoreController.java
@@ -10,10 +10,28 @@
 
 package com.springbootcore.springbootcore.controller;
 
+import com.springbootcore.springbootcore.serviceimpl.SpringBootCoreServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class SpringBootCoreController {
 
 
+	private final SpringBootCoreServiceImpl springBootCoreService;
+
+	public SpringBootCoreController(SpringBootCoreServiceImpl springBootCoreService) {
+		super();
+		this.springBootCoreService = springBootCoreService;
+	}
+
+	@GetMapping(value = {"", "/", " ", "/springbootcore"})
+	public String handleRequests( Model model) {
+
+		model.addAttribute("projectDetails", springBootCoreService.getSpringCoreBeanDetails(1L));
+
+		return "projectDetails";
+	}
 }
