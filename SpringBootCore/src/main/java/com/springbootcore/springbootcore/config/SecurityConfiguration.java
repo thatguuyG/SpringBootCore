4de9b92bb8710056c753009101bc0fcf099a0be2
dk <workplace.davidkariuki@gmail.com>
2022-06-02 17:02:10 +0300

    Updated security configuration

--- SpringBootCore/src/main/java/com/springbootcore/springbootcore/config/SecurityConfiguration.java
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity(debug = true)
@@ -21,7 +22,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated()
+		http.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/springbootcore/**").permitAll()
+				.antMatchers("/index").permitAll()
+				.anyRequest().authenticated()
 				.and().csrf().disable();
 	}
 }
