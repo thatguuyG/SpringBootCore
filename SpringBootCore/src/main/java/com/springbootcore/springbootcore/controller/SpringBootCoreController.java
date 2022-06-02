6349cc572b0fb7fe1e7b04df448d0e3783254ded
dk <workplace.davidkariuki@gmail.com>
2022-06-02 12:53:58 +0300

    Created controller class

---
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/controller/SpringBootCoreController.java
@@ -0,0 +1,19 @@
+/**
+ * Controller Class
+ *
+ * @author - David Kariuki
+ * @version - 1.0.0
+ * @apiNote - Spring project to Re-visit SpringBoot Concepts.
+ * @copyright - Public Domain
+ * @since - 2/6/2022
+ */
+
+package com.springbootcore.springbootcore.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SpringBootCoreController {
+
+
+}
