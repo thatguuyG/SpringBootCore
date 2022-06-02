a9207ebdefa0df4213f41ceb617447027b67b1f9
dk <workplace.davidkariuki@gmail.com>
2022-06-02 14:25:42 +0300

    Added methods to register and get project details

--- SpringBootCore/src/main/java/com/springbootcore/springbootcore/service/SpringBootCoreService.java
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/service/SpringBootCoreService.java
@@ -10,8 +10,25 @@
 
 package com.springbootcore.springbootcore.service;
 
+import com.springbootcore.springbootcore.entity.SpringBootCoreDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface SpringBootCoreService {
+
+	/**
+	 * Interface method to register SpringCore Bean Details
+	 *
+	 * @param springBootCoreDetails - SpringBootCoreDetails Entity
+	 * @return SpringCore
+	 */
+	SpringBootCoreDetails registerSpringCoreBeanDetails(SpringBootCoreDetails springBootCoreDetails);
+
+	/**
+	 * Interface method to get SpringBootCoreDetails details by id
+	 *
+	 * @param id - Id
+	 * @return SpringBootCoreDetails
+	 */
+	SpringBootCoreDetails getSpringCoreBeanDetails(Long id);
 }
