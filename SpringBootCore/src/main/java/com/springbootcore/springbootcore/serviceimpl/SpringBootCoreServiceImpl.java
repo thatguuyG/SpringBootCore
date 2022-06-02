037184d33414f0ead3599c8803507c7cb49c45bc
dk <workplace.davidkariuki@gmail.com>
2022-06-02 13:03:43 +0300

    Created service implementation class

---
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/serviceimpl/SpringBootCoreServiceImpl.java
@@ -0,0 +1,18 @@
+/**
+ * Service Implementation Class
+ *
+ * @author - David Kariuki
+ * @version - 1.0.0
+ * @apiNote - Spring project to Re-Learn SpringBoot Concepts.
+ * @copyright - Public Domain
+ * @since - 31/5/2022
+ */
+
+package com.springbootcore.springbootcore.serviceimpl;
+
+import com.springbootcore.springbootcore.service.SpringBootCoreService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringBootCoreServiceImpl implements SpringBootCoreService {
+}
