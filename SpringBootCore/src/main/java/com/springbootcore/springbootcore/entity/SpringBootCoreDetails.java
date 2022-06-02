31d27adf955900fa5d196b25b35c9b2eafda7991
dk <workplace.davidkariuki@gmail.com>
2022-06-02 12:56:49 +0300

    Created entity class for project details.

---
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/entity/SpringBootCoreDetails.java
@@ -0,0 +1,39 @@
+package com.springbootcore.springbootcore.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+public class SpringBootCoreDetails {
+
+	@Getter
+	@Setter
+	@ToString
+	@Entity
+	@RequiredArgsConstructor
+	@Table(name = "springcore")
+	public class SpringCore {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+
+		@Column(name = "projectName")
+		private String projectName;
+
+		@Column(name = "projectStartDate")
+		private String projectStartDate;
+
+		@Column(name = "projectDescription")
+		private String projectDescription;
+
+		@Column(name = "projectLanguage")
+		private String projectLanguage;
+
+		@Column(name = "projectFramework")
+		private String projectFramework;
+	}
+}
