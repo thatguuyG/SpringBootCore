75ab9bed45d3f7e79053f69c55685ac8c72baf33
dk <workplace.davidkariuki@gmail.com>
2022-06-02 13:01:44 +0300

    Created service class

---
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/service/SpringBootCoreService.java
@@ -0,0 +1,17 @@
+/**
+ * Service Class
+ *
+ * @author - David Kariuki
+ * @version - 1.0.0
+ * @apiNote - Spring project to Re-Learn SpringBoot Concepts.
+ * @copyright - Public Domain
+ * @since - 2/6/2022
+ */
+
+package com.springbootcore.springbootcore.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringBootCoreService {
+}
