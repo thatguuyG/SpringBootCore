28215d1e172d1a2739a0b50e76e750d1c875d117
dk <workplace.davidkariuki@gmail.com>
2022-06-02 12:41:32 +0300

    Initial commit

---
+++ SpringBootCore/src/test/java/com/springbootcore/springbootcore/SpringBootCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootcore.springbootcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
