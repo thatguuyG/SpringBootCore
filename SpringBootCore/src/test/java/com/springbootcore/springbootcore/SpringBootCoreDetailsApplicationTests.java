79bce2fbb169a57e97d49953de721ecac93eb4db
dk <workplace.davidkariuki@gmail.com>
2022-06-02 13:18:15 +0300

    Application Test class

--- SpringBootCore/src/test/java/com/springbootcore/springbootcore/SpringBootCoreDetailsApplicationTests.java
+++ SpringBootCore/src/test/java/com/springbootcore/springbootcore/SpringBootCoreDetailsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootCoreApplicationTests {
+class SpringBootCoreDetailsApplicationTests {
 
 	@Test
 	void contextLoads() {
