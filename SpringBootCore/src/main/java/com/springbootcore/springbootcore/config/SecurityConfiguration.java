fa251de45d5a71a3d4376b769c73d4fded7a14cd
David Kariuki <dkaris.k@gmail.com>
2022-06-03 09:47:58 +0300

    Updated security configuration

--- SpringBootCore/src/main/java/com/springbootcore/springbootcore/config/SecurityConfiguration.java
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/").permitAll()
-				.antMatchers("/springbootcore/**").permitAll()
+				.antMatchers("/springbootcore/api/**").permitAll()
 				.antMatchers("/index").permitAll()
 				.anyRequest().authenticated()
 				.and().csrf().disable();
