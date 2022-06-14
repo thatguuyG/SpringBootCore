42523ec676c87e47632e2e6bb2ba7fdad0e8030d
David Kariuki <dkaris.k@gmail.com>
2022-06-14 14:12:50 +0300

    Minor changes

--- SpringBootCore/src/main/java/com/springbootcore/springbootcore/exception/ResourceNotFoundException.java
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/exception/ResourceNotFoundException.java
@@ -31,10 +31,9 @@ public class ResourceNotFoundException extends RuntimeException {
 	 * @param fieldValue   - FieldValue
 	 */
 	public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
-		super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+		super(String.format("%s not found with %s : '%s", resourceName, fieldName, fieldValue));
 		this.resourceName = resourceName;
 		this.fieldName = fieldName;
 		this.fieldValue = fieldValue;
 	}
-
 }
