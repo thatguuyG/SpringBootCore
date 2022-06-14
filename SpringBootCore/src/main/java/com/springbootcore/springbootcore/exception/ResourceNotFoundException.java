36a498ca714925bd28182fee5654d9c180d55b4a
David Kariuki <dkaris.k@gmail.com>
2022-06-14 14:13:17 +0300

    Minor changes

--- SpringBootCore/src/main/java/com/springbootcore/springbootcore/exception/ResourceNotFoundException.java
+++ SpringBootCore/src/main/java/com/springbootcore/springbootcore/exception/ResourceNotFoundException.java
@@ -15,11 +15,14 @@ import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 @Getter
 @Setter
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
+	@Serial
 	private static final long serialVersionUID = 1L;
 	private String resourceName, fieldName, fieldValue;
 
